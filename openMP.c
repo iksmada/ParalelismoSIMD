b8a31b35aa9abe34544e9714548176e257d34f78
brunowilliamsap <brunowilliamsap@gmail.com>
2015-07-04 17:45:55 -0300

    atualizacao openMP

--- openMP.c
+++ openMP.c
@@ -14,90 +14,93 @@ to see the file use external application ( graphic viewer)
 */
 #include <stdio.h>
 #include <math.h>
-#include <omp.h>
 #include <time.h>
+#include <omp.h>
 
-int main()
-{
-        clock_t start, end;
-		double cpu_time_used;
-        /* screen ( integer) coordinate */
-        int iX,iY;
-        const int iXmax = 16384;
-        const int iYmax = 16384;
-        /* world ( double) coordinate = parameter plane*/
-        double Cx,Cy;
-        const double CxMin=-2.5;
-        const double CxMax=1.5;
-        const double CyMin=-2.0;
-        const double CyMax=2.0;
-        /* */
-        double PixelWidth=(CxMax-CxMin)/iXmax;
-        double PixelHeight=(CyMax-CyMin)/iYmax;
-        /* color component ( R or G or B) is coded from 0 to 255 */
-        /* it is 24 bit color RGB file */
-        const int MaxColorComponentValue=255;
-        FILE * fp;
-        char *filename="_openMP.ppm";
-        static unsigned char color[3];
-        /* Z=Zx+Zy*i  ;   Z0 = 0 */
-        double Zx, Zy;
-        double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
-        /*  */
-        int Iteration;
-        const int IterationMax=256;
-        /* bail-out value , radius of circle ;  */
-        const double EscapeRadius=2;
-        double ER2=EscapeRadius*EscapeRadius;
-        /*create new file,give it a name and open it in binary mode  */
-        fp= fopen(filename,"wb"); /* b -  binary mode */
-        /*write ASCII header to the file*/
-        fprintf(fp,"P6\n %d\n %d\n %d\n",iXmax,iYmax,MaxColorComponentValue);
-        /* compute and write image data bytes to the file*/
-        start = clock();
-	   for(iY=0;iY<iYmax;iY++)
-        {
-             Cy=CyMin + iY*PixelHeight;
-             if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
+int main(){
+ 
+    clock_t start, end;
+    double cpu_time_used;
+    /* screen ( integer) coordinate */
+    int iX,iY;
+    const int iXmax = 16384;
+    const int iYmax = 16384;
+    /* world ( double) coordinate = parameter plane*/
+    double Cx,Cy;
+    const double CxMin=-2.5;
+    const double CxMax=1.5;
+    const double CyMin=-2.0;
+    const double CyMax=2.0;
+    /* */
+    double PixelWidth=(CxMax-CxMin)/iXmax;
+    double PixelHeight=(CyMax-CyMin)/iYmax;
+    /* color component ( R or G or B) is coded from 0 to 255 */
+    /* it is 24 bit color RGB file */
+    const int MaxColorComponentValue=255;
+    FILE * fp;
+    char *filename="_original.ppm";
+    static unsigned char color[3];
+    /* Z=Zx+Zy*i  ;   Z0 = 0 */
+    double Zx, Zy;
+    double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
+    /*  */
+    int Iteration;
+    const int IterationMax=256;
+    /* bail-out value , radius of circle ;  */
+    const double EscapeRadius=2;
+    double ER2=EscapeRadius*EscapeRadius;
+    /*create new file,give it a name and open it in binary mode  */
+    fp= fopen(filename,"wb"); /* b -  binary mode */
+    /*write ASCII header to the file*/
+    fprintf(fp,"P6\n %d\n %d\n %d\n",iXmax,iYmax,MaxColorComponentValue);
+    /* compute and write image data bytes to the file*/
+    start = clock();
+		
+    for(iY=0;iY<iYmax;iY++)
+    {
+	   Cy=CyMin + iY*PixelHeight;
+	   if (fabs(Cy)< PixelHeight/2) 
+		  Cy=0.0; /* Main antenna */
+#pragma omp parallel for
+	   for(iX=0;iX<iXmax;iX++)
+	   {
+		  Cx=CxMin + iX*PixelWidth;
+		  /* initial value of orbit = critical point Z= 0 */
+		  Zx=0.0;
+		  Zy=0.0;
+		  Zx2=Zx*Zx;
+		  Zy2=Zy*Zy;
+		  /* */
 
-             for(iX=0;iX<iXmax;iX++)
-             {
-                        Cx=CxMin + iX*PixelWidth;
-                        /* initial value of orbit = critical point Z= 0 */
-                        Zx=0.0;
-                        Zy=0.0;
-                        Zx2=Zx*Zx;
-                        Zy2=Zy*Zy;
-                        /* */
-                        for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
-                        {
-                            Zy=2*Zx*Zy + Cy;
-                            Zx=Zx2-Zy2 +Cx;
-                            Zx2=Zx*Zx;
-                            Zy2=Zy*Zy;
-                        };
-                        /* compute  pixel color (24 bit = 3 bytes) */
-                        if (Iteration==IterationMax)
-                        { /*  interior of Mandelbrot set = black */
-                           color[0]=0;
-                           color[1]=0;
-                           color[2]=0;
-                        }
-                        else
-                        { /* exterior of Mandelbrot set = white */
-                             color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
-                             color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */
-                             color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
-                        };
-                        /*write color to the file*/
-                        fwrite(color,1,3,fp);
-                }
-        }
+		  for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
+		  {
+			 Zy=2*Zx*Zy + Cy;
+			 Zx=Zx2-Zy2 +Cx;
+			 Zx2=Zx*Zx;
+			 Zy2=Zy*Zy;
+		  };
+		  /* compute  pixel color (24 bit = 3 bytes) */
+		  if (Iteration==IterationMax)
+		  { /*  interior of Mandelbrot set = black */
+			 color[0]=0;
+			 color[1]=0;
+			 color[2]=0;
+		  }
+		  else
+		  { /* exterior of Mandelbrot set = white */
+			 color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
+			 color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */
+			 color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
+		  };
+		  fwrite(color,1,3,fp);
+		
+	   }
+		   
+    }
+    end = clock();
 
-        end = clock();
-
-		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-		printf("time = %f seconds\n", cpu_time_used);
-        fclose(fp);
-        return 0;
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("time = %f seconds\n", cpu_time_used);
+    fclose(fp);
+    return 0;
 }
